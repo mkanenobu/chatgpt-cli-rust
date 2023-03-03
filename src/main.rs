44e3f0ce1bc3f962cf92ee2156021c319a011043
mkanenobu <mitsuru.kanenobu@gmail.com>
2023-03-04 00:01:07 +0900

    Use clap

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ mod set_api_key_prompt;
 use crate::evaluator::Evaluator;
 use crate::repl::start_repl;
 use crate::set_api_key_prompt::set_api_key_prompt;
+use clap::Parser;
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     let conf = config::Config::new();
 
-    if let Some(api_key) = conf.api_key {
+    if args.set_api_key {
+        set_api_key_prompt();
+    } else if let Some(api_key) = conf.api_key {
         let client = openai::client(api_key);
         let evaluator = Evaluator::new(&client);
 
@@ -21,4 +25,12 @@ async fn main() {
         println!("API Key is not set.");
         set_api_key_prompt();
     }
+}
+
+#[derive(Parser, Debug)]
+#[command(version)]
+pub struct Args {
+    /// Set API Key
+    #[arg(long)]
+    set_api_key: bool,
 }
