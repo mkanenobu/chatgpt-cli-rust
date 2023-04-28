01917ae1d9c78ee8986b7e862713779febf1b19a
mkanenobu <kaneda.1018@gmail.com>
2023-04-28 12:19:30 +0900

    Revert "Change config file format from JSON to YAML"
    This reverts commit 93aec02d8a87cbdfaa8c886f5d0b167a0c782583.

--- src/config.rs
+++ src/config.rs
@@ -1,21 +1,29 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
-use std::io::BufReader;
 use std::path::PathBuf;
 use std::{env, fs};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
+    #[serde(rename = "apiKey")]
     pub api_key: Option<String>,
+
+    #[serde(rename = "systemContext")]
     pub system_context: Option<String>,
+
+    #[serde(rename = "model")]
     pub model: Option<String>,
+
+    #[serde(rename = "temperature")]
     pub temperature: Option<f32>,
+
+    #[serde(rename = "topP")]
     pub top_p: Option<f32>,
 }
 
 impl Config {
     pub fn new() -> Result<Config> {
-        read_config_by_yaml_file()
+        read_config_by_json_file()
     }
 
     pub fn default() -> Config {
@@ -29,25 +37,25 @@ impl Config {
     }
 }
 
-fn read_config_by_yaml_file() -> Result<Config> {
+fn read_config_by_json_file() -> Result<Config> {
     let path = get_config_filepath();
-    let reader = BufReader::new(fs::File::open(path)?);
-    let config: Config = serde_yaml::from_reader(reader)?;
+    let config_string = fs::read_to_string(path)?;
+    let config: Config = serde_json::from_str(&config_string)?;
     Ok(config)
 }
 
-// ~/.config/chatgpt-repl/config.yaml
+// ~/.config/chatgpt-repl/config.json
 pub fn get_config_filepath() -> PathBuf {
     let mut path = PathBuf::from(env::var("HOME").unwrap());
     path.push(".config");
     path.push("chatgpt-repl");
-    path.push("config.yaml");
+    path.push("config.json");
     path
 }
 
 fn write_config(config: Config) -> Result<()> {
     let path = get_config_filepath();
-    let config_string = serde_yaml::to_string(&config)?;
+    let config_string = serde_json::to_string_pretty(&config)?;
 
     fs::create_dir_all(path.parent().unwrap())?;
     fs::write(path, config_string)?;
