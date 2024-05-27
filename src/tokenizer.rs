8ad7f531008bc4b2de3be7173b6a00d9fb21bfa5
mkanenobu <mitsuru.kanenobu@gmail.com>
2024-05-27 10:23:17 +0900

    Update tiktoken and remove patch for tiktoken compatibility

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,12 +1,7 @@
 use tiktoken_rs::get_bpe_from_model;
 
 pub fn get_text_token_count(model: &str, text: &str) -> usize {
-    let mut m = model;
-    if m.starts_with("gpt-4o") {
-        // if model is gpt-4o then use gpt-4, tiktoken-rs does not support gpt-4o
-        m = "gpt-4"
-    }
-    let encoder = get_bpe_from_model(m).unwrap();
+    let encoder = get_bpe_from_model(model).unwrap();
     let tokens = encoder.encode_with_special_tokens(text);
     tokens.len()
 }
